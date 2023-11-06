1516e83c1bea74d6ff95c1e0adc7d083a8751da4
xdcron <xdcron95@gmail.com>
2023-11-06 20:57:06 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
